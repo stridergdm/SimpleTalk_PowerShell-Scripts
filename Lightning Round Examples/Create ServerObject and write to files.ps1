a5e63c1b3851edf55308148432234b390a033441
Greg Moore <mooregr@quicr.net>
2020-02-12 09:40:10 -0500

    Lightning Round Examples.

---
+++ Lightning Round Examples/Create ServerObject and write to files.ps1
@@ -0,0 +1,41 @@
+﻿$filepath_text = "$env:HOMEDRIVE$env:HOMEPATH\sqlserverobjectlist.txt" 
+$filepath_json = "$env:HOMEDRIVE$env:HOMEPATH\sqlserverobjectlist.json" 
+
+
+"[" | Out-File -FilePath $filepath_json
+
+$serverObject = New-Object -TypeName psobject
+$serverObject | Add-Member -MemberType NoteProperty -Name ComputerName -Value "one"
+$serverObject | Add-Member -MemberType NoteProperty -Name Environment -Value "PROD"
+$serverObject | Add-Member -MemberType NoteProperty -Name SQLVersion -Value "2016"
+$serverObject | Add-Member -MemberType NoteProperty -Name OSVersion -Value "2012"
+$serverObject | Add-Member -MemberType NoteProperty -Name Products -Value @("FOO","BAR")
+
+$serverObject | out-file -FilePath $filepath_text
+ConvertTo-Json $serverObject | out-file -FilePath $filepath_json -Append
+
+
+"," | Out-File -FilePath $filepath_json -Append
+$serverObject = New-Object -TypeName psobject
+$serverObject | Add-Member -MemberType NoteProperty -Name ComputerName -Value "TWO"
+$serverObject | Add-Member -MemberType NoteProperty -Name Environment -Value "DR"
+$serverObject | Add-Member -MemberType NoteProperty -Name SQLVersion -Value "2016"
+$serverObject | Add-Member -MemberType NoteProperty -Name OSVersion -Value "2012"
+$serverObject | Add-Member -MemberType NoteProperty -Name Products -Value @("FOO","BAR")
+
+$serverObject | out-file -FilePath $filepath_text -Append
+ConvertTo-Json $serverObject | out-file -FilePath "$env:HOMEDRIVE$env:HOMEPATH\sqlserverobjectlist.json" -Append
+
+"," | Out-File -FilePath $filepath_json -Append
+$serverObject = New-Object -TypeName psobject
+$serverObject | Add-Member -MemberType NoteProperty -Name ComputerName -Value "THREE"
+$serverObject | Add-Member -MemberType NoteProperty -Name Environment -Value "PROD"
+$serverObject | Add-Member -MemberType NoteProperty -Name SQLVersion -Value "2019"
+$serverObject | Add-Member -MemberType NoteProperty -Name OSVersion -Value "2012"
+$serverObject | Add-Member -MemberType NoteProperty -Name Products -Value @("FIZ","BIN","BAZ")
+
+$serverObject | out-file -FilePath $filepath_text -Append
+ConvertTo-Json $serverObject | out-file -FilePath "$env:HOMEDRIVE$env:HOMEPATH\sqlserverobjectlist.json" -Append
+
+
+"]" | Out-File -FilePath $filepath_json -Append
---
+++ Lightning Round Examples/Server Script with passed in parameter.ps1
@@ -0,0 +1,33 @@
+﻿# Presented as a quick demo for a lightning round.
+# This is NOT productin quality code!
+# As was pointed out to me after the user group meeting, there are a number of improvements one could make to the code.
+# As they say in college, I leave those as an exercise for the reader.
+# (Though a hint, one could use a WHERE search instead of the IF)
+param([string]$environment, $version)
+
+    $serverobjlist = Get-Content -Raw -Path "$env:HOMEDRIVE$env:HOMEPATH\sqlserverobjectlist.json" | ConvertFrom-Json
+
+if ($environment -eq "" -and $version -ne "")
+{
+    foreach ($server in $serverobjlist)
+    {
+    
+        if ($version -eq $server.SQLVersion)
+        {
+                    Write-Host "The following operation will be done on server $($Server.ComputerName) in the $($Server.Environment) Environment which is running SQL Server Version: $version"
+        }
+    }
+    return
+}
+else
+
+{
+    foreach ($server in $serverobjlist)
+    {
+    
+        if ($environment -eq $server.Environment)
+        {
+                    Write-Host "The following operation will be done on server $($Server.ComputerName) in the $($Server.Environment) Environment"
+        }
+    }
+}
