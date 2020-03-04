6a4a857a85f8f3fc7ad289baabf67f7f334a46fe
Greg Moore <mooregr@quicr.net>
2020-03-04 08:48:42 -0500

    Initial push for Introducing Visual Studio Code

---
+++ More Parameters and Help/Boolean_help.ps1
@@ -0,0 +1,15 @@
+param([boolean] $help )
+#
+# Author: Greg D. Moore
+# Date: 2020-02-18
+# Version: 1.0
+# Show boolean help
+#
+if ($help -eq $true)
+{
+    write-host "This is help for this program. It does nothing. Hope that helps."
+}
+else
+{
+    write-host "Do nothing."
+}
