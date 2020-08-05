859562bd1ec19b8d5e9ada3fc3b730a637d06694
Greg Moore <mooregr@greenms.com>
2020-08-05 09:22:59 -0400

    Added validateset scripts

---
+++ Parameters Part II/ValidateSet_Caveat_1.ps1
@@ -0,0 +1,11 @@
+﻿param([parameter(mandatory)][validateset('Good','Bad')] $status)
+
+Write-host "Status is $status"
+
+$status = 'Ugly'
+
+Write-host "Status is still $status"
+
+$status = 'Good'
+
+Write-host "Status is now $status"
---
+++ Parameters Part II/ValidateSet_Caveat_2.ps1
@@ -0,0 +1,9 @@
+﻿param([validateset('Lorca','Georgiou','Pike','Kirk')] $Captain, $color)
+
+[validateSet('red','yellow')] $alert = 'red'
+[validateSet('good','bad')] $status
+
+Write-host "Captain $Captain has declared $alert alert and our status is $status!"
+$alert = $color
+$status = 'REALLY BAD'
+Write-host "Now Captain $Captain has declared $alert alert and our status is $status!"
